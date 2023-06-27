1c47aeb349bb4a61e944f15b6ff9ca594e8ec02f
MAZULYTIAX <emiclothing89@gmail.com>
2023-06-27 11:28:37 +0100

    seminar week 1 completed

---
+++ basics/output/escape_characters.py
@@ -0,0 +1,6 @@
+#Displays escape characters
+print("\n displays a new line")
+print("\t displays a tab")
+print("\\ displays a backslash")
+print("\" displays a double qoute")
+print("\' displays a single qoute")
